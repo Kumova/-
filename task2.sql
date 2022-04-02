41308931025ec5d202b3c38f48a7597e3d3cb8f4
Daria <you@example.com>
2022-04-02 18:11:28 +0300

    изменен

--- task2.sql
+++ task2.sql
@@ -2,24 +2,26 @@ create database musikshop with owner kumova;
 
 create table if not exists Musician ( 
       Id serial primary key,
-       nickname varchar(40) not null);
+      nickname varchar(40) not null,
+      genre_id integer
+      );
 
 create table if not exists Album (
     Id serial primary key, 
     name varchar(40) not null,
-    year_of_releas date,
-   musician_id integer references Musician(id)
+    year_of_releas integer,
+    musician_id integer references Musician(id)
    );
 
 create table if not exists Track (
          Id serial primary key,
          name varchar(40) not null, 
-         time time, 
+         time integer, 
          album_id integer References Album(id)
    );
     
 create table if not exists Genre ( 
      Id serial primary key, 
      name_genre varchar(40) not null,
-     musician_id integer References Musician(id)
-);  
+     musician_id integer References Musician(genre_id)
+); 
